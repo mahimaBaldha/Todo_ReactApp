9a7a040eca938e7c94dc0a5535a5133f522a1159
mb1997 <mahima.baldha04@gmail.com>
2020-07-23 21:41:48 +0530

    Basic Todo App

---
+++ src/components/AddTodo.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddTodo extends Component {
+    state = {
+        title: ''
+    }
+
+    onChange = (e) => this.setState({
+        [e.target.name]: e.target.value
+    });
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.addTodo(this.state.title);
+        this.setState({title:''});
+    }
+
+    render() {
+        return (
+        <form onSubmit={this.onSubmit} style={{ display: 'flex' }}>
+            <input 
+                type="text" 
+                name="title" 
+                placeholder="Add Todo ..."
+                value={this.state.title}
+                onChange={this.onChange}
+                style={{flex: 8, padding:'10px'}}/>
+            <input 
+                type="submit" 
+                value="submit" 
+                className="btn"
+                style={{flex: 1}}
+                />
+        </form>
+        );
+    }
+}
+
+AddTodo.propTypes = {
+    todo: PropTypes.object.isRequired
+}
+
+export default AddTodo;
